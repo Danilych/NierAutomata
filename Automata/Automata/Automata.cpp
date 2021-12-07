78b19b71f39107e9225886510eaa22d9dfad9465
astyd256 <astyd637@gmail.com>
2021-12-07 19:09:04 +0700

    Cleaning MICROSOFT GARBAGE

--- Automata/Automata/Automata.cpp
+++ Automata/Automata/Automata.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    std::cout << "ïîãäîsdgsdgsè!\n";
+    std::cout << "ïîãäîsdgsdgsèsss!!!!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
