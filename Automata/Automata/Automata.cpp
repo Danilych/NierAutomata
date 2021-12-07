b020df0a7d435c283eca878b56b35e384b84b5ed
Nixion <den-kosarev@mail.ru>
2021-12-07 18:48:33 +0700

    com

--- Automata/Automata/Automata.cpp
+++ Automata/Automata/Automata.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "îëäîëäîäëssssûûû!\n";
+    setlocale(LC_ALL, "Russian");
+    std::cout << "ïîãäîsdgsdgsè!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
